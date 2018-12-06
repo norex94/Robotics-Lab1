681a4caa7e282f235f02420229beaccc81021a11
Hjorvarv <hjorvar16@ru.is>
2018-12-06 08:55:16 +0000

    Finish

--- main.cpp
+++ main.cpp
@@ -19,26 +19,27 @@ int main()
 	Taskspace TaskNext{ 1,1,1,0,0,0 };
 	
 	robot.InverseKinematics(TaskCurrent, JointCurrent);
+	char yes = 0;
+	bool out = true;
 
-	
-	while (1) {
+	while (out) {
 		printf("Please insert steps: \n");
-		printf("X");
+		printf("X: ");
 		scanf("%lf", &TaskNext.x);
 		scanf(", ");
-		printf("Y");
+		printf("Y: ");
 		scanf("%lf", &TaskNext.y);
 		scanf(", ");
-		printf("Z");
+		printf("Z: ");
 		scanf("%lf", &TaskNext.z);
 		scanf(", ");
-		printf("P");
+		printf("P: ");
 		scanf("%lf", &TaskNext.p);
 		scanf(", ");
-		printf("R");
+		printf("R: ");
 		scanf("%lf", &TaskNext.r);
 		scanf(", ");
-		printf("G");
+		printf("G: ");
 		scanf("%lf", &TaskNext.g);
 		TaskNext.p = TaskNext.p*PI / 180;
 		TaskNext.r = TaskNext.r*PI / 180;
@@ -51,15 +52,29 @@ int main()
 		}
 		robot.JointToRegister(JointNext, Steps);
 		robot.SendStep(spe, Steps);
-
-
-
-
+		printf("Continue? y/n \n");
+		scanf(" %c", &yes);
+		if (yes == 'n')
+		{
+			out = false;
+		}
+		
 	}
-	
-	
-
-
+	robot.SendClose(221, 1);
+	TaskNext.x = 125;
+	TaskNext.y = 0;
+	TaskNext.z = 0;
+	TaskNext.p = -90 * PI / 180;
+	TaskNext.r = 0;
+	robot.InverseKinematics(TaskNext, JointNext);
+	for (int i = 1; i < 6; i++)
+	{
+		JointNext.t[i] = JointCurrent.t[i] - JointNext.t[i];
+		JointCurrent.t[i] = JointCurrent.t[i] - JointNext.t[i];
+	}
+	robot.JointToRegister(JointNext, Steps);
+	robot.SendStep(spe, Steps);
+	printf("Thank you goodbye");
 
 
 }
